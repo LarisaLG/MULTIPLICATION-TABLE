704320ff9d573c9808b73725e1558ff2f53c3720
LarisaLG <lgalisheva@gmail.com>
2022-06-13 19:38:53 +0000

    add functions to show multipliers, to calculate result, to count scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,13 +19,42 @@ document.addEventListener("DOMContentLoaded", function () {
  *assigns its value to the variable to  the first multiplier, 
  * then outputs Multiplier2 and calculate result
  */
-function showshowMult1() {}
+function showshowMult1() {
+    let num1 = parseInt(this.innerHTML);
+    //console.log(num1);
+
+    //assigns choosen number value to the first Multiplier
+    //let multiplier1 = document.getElementById('operand1'); 
+    multiplier1.innerHTML = num1;
+    showMult2();
+}
+
 
 //outputs  Multiplier2
-function showMult2() {}
+function showMult2() {
+    let num2 = 1;
+    function delay() {
+        multiplier2.innerHTML = num2;
+        console.log("Multiplier2 is " + multiplier2.innerHTML);
+        
+        num2++;
+
+        //checkAnswer();   check which button is clicked
+        if(num2 <= numbers.length ) {
+            setTimeout(delay, 3000);
+        }
+    }
+    delay();
+    }
+
 
 //calculates correct multiplication result
-function calcResult() {}
+function calcResult() {
+    num1 = parseInt(multiplier1.innerHTML);
+    num2 = parseInt(multiplier2.innerHTML);
+    return num1 * num2;
+}
+
 
 // function which outputs random answers for user
 function showAnswers() {}
@@ -38,10 +67,16 @@ function checkAnswer() {}
 /**
  * Gets the current score and increments it by 1
  */
-function countScores() {}
+function countScores() {
+    let score = parseInt(document.getElementById('correct').innerText);
+    document.getElementById('correct').innerText = ++score;
+}
 
 
 /**
  * Gets incorrect answers from the DOM and increments it by 1
  */
-function countErrors() {}
+function countErrors() {
+    let score = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++score;
+}
